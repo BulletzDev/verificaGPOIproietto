f6d731469d5f5388dd70df6a3cea9cfbc6186570
BulletzDev <gabri.proietto2007@gmail.com>
2026-02-09 11:16:41 +0100

    Contobancario class and its javadoc

--- verificaproietto/src/main/java/it/proietto/Main.java
+++ verificaproietto/src/main/java/it/proietto/Main.java
@@ -2,6 +2,14 @@ package it.proietto;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ContoBancario conto = new ContoBancario("IT60X0542811101000000123456", 1000.0);
+        System.out.println("IBAN: " + conto.getIban());
+        System.out.println("Saldo iniziale: " + conto.getSaldo());
+
+        conto.deposita(500.0);
+        System.out.println("Saldo dopo deposito: " + conto.getSaldo());
+
+        conto.preleva(200.0);
+        System.out.println("Saldo dopo prelievo: " + conto.getSaldo());
     }
 }
